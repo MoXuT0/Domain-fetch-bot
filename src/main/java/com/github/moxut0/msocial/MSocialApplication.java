a53ecc176f9486032067484779789e94e85f8252
MoXuT0 <DenisFisenko2010@yandex.ru>
2023-09-03 13:59:50 +0300

    Initial commit

---
+++ src/main/java/com/github/moxut0/msocial/MSocialApplication.java
@@ -0,0 +1,13 @@
+package com.github.moxut0.msocial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MSocialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MSocialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/moxut0/msocial/MSocialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.moxut0.msocial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MSocialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
